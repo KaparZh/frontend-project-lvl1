5692c764cb63d23cd519c9b4bb96bc804f16d20d
KaparZh <andkapar@gmail.com>
2021-04-27 04:08:27 +0600

    big update

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const name = () => {
-  console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question("May I have your name?: ");
+const name = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}`);
 };
+
+export default name;
