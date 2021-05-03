2f3b434fe6db52cbd5c71cbe4330023a464db07c
KaparZh <andkapar@gmail.com>
2021-05-04 00:49:47 +0600

    add even.js brain-even.js

---
+++ src/even.js
@@ -0,0 +1,44 @@
+import readlineSync from 'readline-sync';
+import name from './cli.js';
+
+const evenGame = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const questionNum = Math.floor(Math.random() * 100);
+    console.log(`Question: ${questionNum}`);
+    const answer = readlineSync.question('Your answer: ');
+    let isCorrect = false;
+
+    if (questionNum % 2 === 0 && answer === 'yes') {
+      isCorrect = true;
+    }
+    if (questionNum % 2 !== 0 && answer === 'no') {
+      isCorrect = true;
+    }
+
+    if (isCorrect) {
+      console.log('Correct!');
+    }
+    if (!isCorrect && answer === 'yes') {
+      console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
+      console.log(`Let's try again, ${name.userName}!`);
+      break;
+    }
+    if (!isCorrect && answer === 'no') {
+      console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.');
+      console.log(`Let's try again, ${name.userName}!`);
+      break;
+    }
+    if (!isCorrect) {
+      console.log('Incorrect! Enter "yes" or "no"!');
+      break;
+    }
+
+    if (i === 2) {
+      console.log(`Congratulations, ${name.userName}`);
+    }
+  }
+};
+
+export default evenGame;
